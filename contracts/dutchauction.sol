c3da02ba7abb89c0ec72781dec7b61aa04232faf
felix-adigbli <adigbli.f@northeastern.edu>
2023-06-25 14:02:12 -0400

    new update pushed

--- contracts/dutchauction.sol
+++ contracts/dutchauction.sol
@@ -11,8 +11,10 @@ contract DutchAuction {
     uint256 public currentPrice;
     bool public auctionEnded;
     address public bidWinner;
+    // Event emitted when bid is processed successfully
+    event BidProcessed(address indexed bidder, uint256 amount);
 
-    //Seller Placed A bid
+    // Seller Placed A bid
 
     constructor(
         uint256 _reservePrice,
@@ -28,29 +30,38 @@ contract DutchAuction {
         auctionEnded = false;
     }
 
-        function getCurrentPrice() public returns (uint256) {
-        currentPrice =
-            reservePrice +
-            (endBlock - block.number) *
-            offerPriceDecrement;
-        return (currentPrice);
+    function getCurrentPrice() public returns (uint256) {
+        if (endBlock < block.number) {
+            currentPrice = reservePrice;
+            return currentPrice;
+        } else {
+        currentPrice = reservePrice + (endBlock - block.number) * offerPriceDecrement;
+        return currentPrice;
         }
+
+
+    }
 
     // Function to get the bid winner
     function getBidWinner() public view returns (address) {
         return bidWinner;
     }
 
-    //function for bidders to place bid and proccess the bid
+    // Function for bidders to place bid and process the bid
     function placeBid() external payable {
         require(!auctionEnded, "Auction has ended");
-        getCurrentPrice();     //get current price
+        getCurrentPrice(); // Get current price
+        require(block.number <= endBlock, "Auction has expired");
         if (msg.value >= currentPrice) {
             auctionEnded = true;
-            seller.transfer(msg.value); //Transfer bid to seller
+            seller.transfer(msg.value); // Transfer bid to seller
             bidWinner = msg.sender;
+            emit BidProcessed(msg.sender, msg.value); // Emit the BidProcessed event
+
         } else {
-            payable(msg.sender).transfer(msg.value); //Transfer bid to sender
+            payable(msg.sender).transfer(msg.value); // Transfer bid to sender
+
         }
     }
+
 }
